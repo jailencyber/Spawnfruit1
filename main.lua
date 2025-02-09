0ab82867ab055886e33e9e025119f98ac45aba5f
jailencyber <leotempapi@gmail.com>
2025-02-09 14:18:58 -0600

    Update

---
+++ main.lua
@@ -0,0 +1,12 @@
+-- Example of a simple Lua script
+
+-- Print a message to indicate that the script has loaded successfully
+print("main.lua has been loaded successfully.")
+
+-- Define a simple function
+function greet(name)
+    print("Hello, " .. name .. "!")
+end
+
+-- Call the function with a sample name
+greet("world")
